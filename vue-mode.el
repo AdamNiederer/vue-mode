4637d926096b0fc6895d5ac024a2ef17a6b513f8
Adam <adam.niederer@gmail.com>
2017-10-13 13:23:41 -0400

    Use mmm-indent-region and mmm-indent-line-narrowed
    Resolves #43 and #50

--- vue-mode.el
+++ vue-mode.el
@@ -199,7 +199,9 @@ open in a window."
 ;;;###autoload
 (define-derived-mode vue-mode html-mode "vue"
   (when (not vue-initialized)
-    (vue--setup-mmm)))
+    (vue--setup-mmm))
+  (setq mmm-indent-line-function #'mmm-indent-line-narrowed)
+  (setq indent-region-function #'mmm-indent-region))
 
 ;;;###autoload
 (setq mmm-global-mode 'maybe)
