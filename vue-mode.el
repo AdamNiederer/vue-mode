48ff04657613f39848d0e66e9dd367aa2dc19e89
Adam Niederer <adam.niederer@gmail.com>
2018-04-28 16:25:54 -0400

    Release 0.4
    - Improve detection of component sections
    - Use narrowed indentation by default
    - Allow indirect editing of all windows
    - Add customizable indirect modes

--- vue-mode.el
+++ vue-mode.el
@@ -5,8 +5,8 @@
 ;; Author: codefalling <code.falling@gmail.com>
 ;; Keywords: languages
 
-;; Version: 0.3.1
-;; Package-Requires: ((mmm-mode "0.5.4") (vue-html-mode "0.1") (ssass-mode "0.1") (edit-indirect "0.1.4"))
+;; Version: 0.4.0
+;; Package-Requires: ((mmm-mode "0.5.5") (vue-html-mode "0.2") (ssass-mode "0.2") (edit-indirect "0.1.4"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
